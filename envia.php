1918dc98f6faae04d48d8bf123966cb28a51b083
Everton Lima <ever-tonlima@hotmail.com>
2018-03-28 00:47:24 -0300

    First commit

---
+++ envia.php
@@ -0,0 +1,38 @@
+<?php
+	header('Content-Type: text/html; charset=UTF-8');
+	header('Content-Type: text/plain; charset=UTF-8');
+	
+	function pegaValor($valor) {
+		return isset($_POST[$valor]) ? $_POST[$valor] :'';
+	}
+
+	$nome = pegaValor("nome");
+	$telefone = pegaValor("tel");
+	$email = pegaValor("email");
+	$mensagem = pegaValor("msg");
+	
+	
+	
+	$quebra_linha = "\n\n";
+
+	$emailSender = "ti@zabo.com.br";
+	$nomeRemetente = "Green ST";
+	$emailDestinatario = "felipe@spicycomm.com.br, David@zabo.com.br, vendas@playimovel.com.br, emily@playimovel.com.br, leadsgreenst@gmail.com";
+	$assunto = "Contato enviado pelo site www.greenst.com.br";
+	$msg= $quebra_linha;
+	$msg .= "Nome: ".$nome.$quebra_linha;
+	$msg .= "Email: ".$email.$quebra_linha;
+	$msg .= "Telefone: ".$telefone.$quebra_linha;
+	$msg .= "Mensagem: " .$mensagem.$quebra_linha;
+	
+	
+	$headers= "MIME-Version: 1.1".$quebra_linha;
+	
+	
+
+	if(mail($emailDestinatario, $assunto, $msg, $headers, "-r".$emailSender)) {
+		header("location: obrigado.html");
+	} else {
+		echo "Não foi possível enviar o email";
+	}
+?>
---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Green Street  </title>
+  	<meta charset="utf-8">
+  	<meta http-equiv="content-language" content="pt-br">
+  	<meta name="keywords" content="Apartamento, Studio, apartamento na pompeia, apartamento em perdizes, pompeia, perdizes, comprar apartamento, lançamento na pompeia, lançamento na pardizes">
+  	<meta name="description" content="Localização nobre, arquitetura sofisticada, e apartamento com muito estilo">
+
+	<link rel="stylesheet" href="css/style.css">
+	<style>
+		::-webkit-input-placeholder  { 
+			font-weight: 100;
+			color: silver;
+		}
+		input:-moz-placeholder { 
+			font-weight: 100; 
+			font-size: 1.5em;
+			color: tomato;
+
+		}
+		textarea:-moz-placeholder {
+		 font-weight: 100;
+		 font-size: 1.5em;
+		color: tomato;
+		}
+	</style>
+</head>
+<body>
+	<main class="main-site container">
+		<div class="logo-topo">
+			<a href="javascript:void(0);"><img src="images/logo-topo.png" alt="Green Street Logo"></a>
+		</div>
+		<div class="section-form">
+			<div class="main-title">
+				<a href="javascript:void(0);">
+					<img src="images/GreenStreet_Fachada.png" alt="A vida no seu tom">
+				</a>
+			</div>
+			
+
+			<form action="envia.php" method="post" class="form-site">
+				<input type="text" placeholder="Nome:" name="nome" required>
+
+				<input type="tel" placeholder="Telefone:" name="tel" required>
+					
+				<input type="email" placeholder="E-mail:" name="email" required>
+
+				<textarea placeholder="Mensagem:" name="msg"></textarea>
+
+				<input type="submit" value="Enviar">
+		</form>
+
+		<div class="subtitle-header-bottom">
+			<p>
+				Receba informações sobre o lançamento mais esperado de Perdizes.
+				Cadastre-se e em breve entraremos em contato com você.
+			</p>
+		</div>
+		</div>
+	</main>
+	
+	<div class="pompeia-fun container">
+		<div class="art-pompeia">
+		</div>
+		<div class="text-section">
+			<h2>Perdizes</h2>
+			<p>Onde a diversão e as comodidades <br>
+			<span>Dão um show.</span>
+			</p>
+			<img src="images/stand-de-vendas.png" alt="">  
+		</div>
+	</div>
+	
+	<section class="section-full view-buildings container">
+		<img src="images/perspectiva-predios.jpg" alt="">
+	</section>
+	
+	<section class="container container-zabo-na-medida">
+		<div class="zabo-sintese">
+			<div class="descricao-medida">
+				<p>
+					<strong>Green ST</strong>. É a <strong>síntese da essência ZABO:<br>
+					localização nobre, arquitetura sofisticada</strong><br>
+					e <strong>apartamentos</strong> com <strong>muito estilo.</strong><br>
+					Tudo na medida para um estilo de vida contemporâneo e descolado. <br>
+					<span><strong>Você merece.</strong></span>
+				</p>
+			</div>
+		
+			<div class="section-slide">
+				<div class="slide-medida">
+					<div>
+						<picture>
+							<!--[if IE 9]><video style="display: none;"><![endif]-->
+  							<source srcset="images/mobile-parque.jpg" media="(max-width: 480px)">
+  							<!--[if IE 9]></video><![endif]-->
+  							<img srcset="images/parque.jpg" alt="">
+						</picture>
+					</div>
+					<div>
+						<picture>
+							<!--[if IE 9]><video style="display: none;"><![endif]-->
+	  						<source srcset="images/mobile-metro.jpg" media="(max-width: 480px)">
+	  						<!--[if IE 9]></video><![endif]-->
+	  						<img srcset="images/metro.jpg" alt="">
+					</picture>
+					</div>
+					<div>
+						<picture>
+							<!--[if IE 9]><video style="display: none;"><![endif]-->
+	  						<source srcset="images/mobile-sesc.jpg" media="(max-width: 480px)">
+	  						<!--[if IE 9]></video><![endif]-->
+	  						<img srcset="images/sesc.jpg" alt="">
+						</picture>
+					</div>
+					<div>
+						<picture>
+							<!--[if IE 9]><video style="display: none;"><![endif]-->
+	  						<source srcset="images/mobile-shopping.jpg" media="(max-width: 480px)">
+	  						<!--[if IE 9]></video><![endif]-->
+	  						<img srcset="images/shopping.jpg" alt="">
+						</picture>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="pompeia-fun zabo-na-medida container">
+			<div class="text-section">
+				<p>UM ZABO NA MEDIDA<br>
+			 	E NO TOM PERFEITO<br>
+					PARA VOCÊ.
+				</p>
+			</div>
+		</div>
+	</section>
+	
+	<div class="data-building container">
+		<img src="images/produto-02.png">
+	</div>
+
+	<section class="life-style life-style-detail container">
+		<div class="text-section">
+			<div class="title-section-style">
+				<h2>Viva seu estilo<br> do melhor jeito.</h2>
+			</div>
+			<div class="description-section-style">
+				<p>
+					Referência em <strong>altíssimo padrão há mais de 30 anos</strong>,<br> a <strong>Zabo</strong>
+					traz para o <strong>Green St</strong>. <br> um estilo de vida contemporâneo e descolado. <br>
+					Tudo criado na medida do seu desejo, como você merece.
+				</p>
+			</div>
+		</div>
+	</section>
+	
+	<section class="suite-section section-img-full container">
+		<div class="suite-section-slide">
+			<div class="suite-slide">
+				<div>
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+  						<source srcset="images/mobile-suite.jpg" media="(max-width: 680px)">
+  						<!--[if IE 9]></video><![endif]-->
+  						<img srcset="images/suite.jpg" alt="">
+					</picture>
+				</div>
+				<div>
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+  						<source srcset="images/mobile-living.jpg" media="(max-width: 680px)">
+  						<!--[if IE 9]></video><![endif]-->
+  						<img srcset="images/living.jpg" alt="">
+					</picture>
+				</div>
+				<div>
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+  						<source srcset="images/mobile-hall.jpg" media="(max-width: 680px)">
+  						<!--[if IE 9]></video><![endif]-->
+  						<img srcset="images/hall.jpg" alt="">
+					</picture>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="life-style section-fun container">
+		<div class="text-section">
+			<h2 class="title-section-style">Diversões para cada estilo.<br>Venha curtir o seu!</h2>
+		</div>
+	</section> 
+	
+	<section class="section-fun-out section-img-full container">
+		<div class="section-fun-slide">
+			<div class="fun-slide">
+				<div>
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+  						<source srcset="images/mobile-piscina.jpg" media="(max-width: 680px)">
+  						<!--[if IE 9]></video><![endif]-->
+  						<img srcset="images/piscina-full.jpg" alt="">
+					</picture>
+				</div>
+				<div>
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+  						<source srcset="images/mobile-sala-de-jogos.jpg" media="(max-width: 680px)">
+  						<!--[if IE 9]></video><![endif]-->
+  						<img srcset="images/sala-de-jogos.jpg" alt="">
+					</picture>
+				</div>
+				<div>
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+  						<source srcset="images/mobile-brinquedoteca.jpg" media="(max-width: 680px)">
+  						<!--[if IE 9]></video><![endif]-->
+  						<img srcset="images/brinquedoteca.jpg" alt="">
+					</picture>
+				</div>
+				<div>
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+  						<source srcset="images/mobile-academia.jpg" media="(max-width: 680px)">
+  						<!--[if IE 9]></video><![endif]-->
+  						<img srcset="images/academia.jpg" alt="">
+					</picture>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="section-plantas container">
+		<h2>Seu espaço merece ter a sua cara.</h2>
+		<div class="section-planta section-planta-1">
+			<h3><strong>37</strong>m<sup>2</sup> - 1 Suíte C/Lavabo</h3>
+			<figure>
+				<img src="images/planta-1.jpg" alt="Planta">
+				<figcaption>
+					<p>
+						Planta ilustrativa do apartamento de 1 dormitório. Os móveis, objetos e revestimentos de piso são sugestões e não fazem parte do memorial descritivo.
+					</p>
+				</figcaption>
+			</figure>
+		</div>
+		<div class="section-planta section-planta-2">
+			<h3><strong>54</strong>m<sup>2</sup> - 2 Dorms. (1 Suíte)</h3>
+			<figure>
+				<img src="images/planta-2.jpg" alt="Planta">
+				<figcaption>
+					<p>
+						Opção de Planta ilustrativa do apartamento de 2 dormitórios com terraço integrado ao living. Os móveis, objetos e revestimentos de piso são sugestões e não fazem parte do memorial descritivo.
+					</p>
+				</figcaption>
+			</figure>
+		</div>
+	</section> 
+
+	<footer class="site-footer container">
+		<a href="javascript:void(0);"><img src="images/logo-rodape.png" alt="Logo Rodapé"></a>
+	</footer>
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="slick/slick.min.js"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ 
+  ga('create', 'UA-106054433-1', 'auto');
+  ga('send', 'pageview');
+ 
+	</script>
+	<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107411357-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+ 
+  gtag('config', 'UA-107411357-1');
+</script>
+
+<!-- Código do Google para tag de remarketing -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 833219621;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/833219621/?guid=ON&amp;script=0"/>
+</div>
+</noscript>
+	<script>
+	$(function() {
+		$(".slide-medida").slick({
+			dots: true
+		});
+
+		$(".fun-slide").slick({
+			dots: true
+		});
+
+		$(".suite-slide").slick({
+			dots: true
+		});
+	})
+	</script>
+</body>
+</html>
